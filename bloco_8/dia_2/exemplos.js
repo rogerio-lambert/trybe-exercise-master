953b66e6e0265855a461f47fc9a275bab88e818d
rogerio <rogerio.cl@gmail.com>
2021-03-30 17:38:31 -0300

    exercicios do dia 2 do bloco 8

---
+++ bloco_8/dia_2/exemplos.js
@@ -0,0 +1,29 @@
+const students = [
+    { name: 'Maria', grade: 70, approved: '' },
+    { name: 'José', grade: 56, approved: '' },
+    { name: 'Roberto', grade: 90, approved: '' },
+    { name: 'Ana', grade: 81, approved: '' },
+  ];
+  
+  function verifyGrades() {
+    students.forEach((student, index) => {
+      if (student.grade >= 60) {
+        student.approved = 'Aprovado';
+      } else {
+        student .approved = 'Recuperação';
+      }
+    });
+  }
+  
+  verifyGrades();
+  
+  console.log(students);
+
+  const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
