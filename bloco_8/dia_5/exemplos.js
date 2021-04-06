3848994d07f80a96826b3a2dd9e2f1f79d621814
rogerio <rogerio.cl@gmail.com>
2021-04-06 20:33:30 -0300

    exercicios e exemplos dos dias 4 e 5 do bloco 8

---
+++ bloco_8/dia_5/exemplos.js
@@ -0,0 +1,42 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['', '', ''];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['', '', ''];
+
+const fruitSalad = (fruit, additional) => {
+  // Esreva sua função aqui
+  return [...fruit, ...additional];
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
+
+const getNationality = ({ firstName, nationality = 'brasilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+    firstName: 'João',
+    lastName: 'Jr II',
+};
+
+const otherPerson = {
+    firstName: 'Ivan',
+    lastName: 'Ivanovich',
+    nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+
+const getPosition = (latitude, longitude) => ({
+    latitude,
+    longitude,});
+  
+  console.log(getPosition(-19.8157, -43.9542));
+
+const multiply = (number, value = 1) => {
+  // Escreva aqui a sua função
+  return number * value;
+};
+  
+console.log(multiply(8, 2));
+
